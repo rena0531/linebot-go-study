469679f2cae3be7389f0dec909180629e6fbdc3c
Rena <choco.rena0609@gmail.com>
2020-04-30 13:34:55 +0900

    環境変数の設定

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/rena/linebot-go-study
 
 go 1.14
 
-require github.com/line/line-bot-sdk-go v7.3.0+incompatible // indirect
+require (
+	github.com/joho/godotenv v1.3.0 // indirect
+	github.com/line/line-bot-sdk-go v7.3.0+incompatible // indirect
+)
--- main.go
+++ main.go
@@ -1,24 +1,33 @@
 package main
 
 import (
+	"github.com/joho/godotenv"
 	"github.com/line/line-bot-sdk-go/linebot"
 	"log"
 	"net/http"
 	"os"
 )
 
-func main(){
+func EnvLoad() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
+func main() {
+	EnvLoad()
 	bot, err := linebot.New(os.Getenv("CHANNEL_SECRET"), os.Getenv("CHANNEL_TOKEN"))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	http.HandleFunc("/callback", func(w http.ResponseWriter, r *http.Request){
+	http.HandleFunc("/callback", func(w http.ResponseWriter, r *http.Request) {
 		events, err := bot.ParseRequest(r)
 		if err != nil {
-			if err == linebot.ErrInvalidSignature{
+			if err == linebot.ErrInvalidSignature {
 				w.WriteHeader(400)
-			}else{
+			} else {
 				w.WriteHeader(500)
 			}
 			return
@@ -33,9 +42,9 @@ func main(){
 				}
 			}
 		}
-
-		if err := http.ListenAndServe(":"+os.Getenv("PORT"), nil); err != nil {
-			log.Fatal(err)
-		}
 	})
+
+	if err := http.ListenAndServe(":"+os.Getenv("PORT"), nil); err != nil {
+		log.Fatal(err)
+	}
 }
